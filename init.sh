5f3e26d70a66430f88f26a1c384052560d884287
dchima <dcnnad@gmail.com>
2020-03-29 21:36:36 +0100

    :circus_tent: added init.sh file

---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# based on these two files
+# https://github.com/afeld/dotfiles/blob/master/init.sh
+# https://github.com/driesvints/dotfiles/blob/master/install.sh
+
+# Die on failures
+set -e
+
+# Echo all commands
+# set -x
+
+echo "Setting up your Mac..."
+
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
+# homebrew
+if command_exists brew; then
+  brew update
+else
+  echo "Installing brew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "...brew installed"
+fi
+
+# install all brew dependencies with brew
+
+brew bundle
+
+# Set default postgress root password.
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+
+# Removes .zshrc && .zprofile from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc $HOME/.zprofile
+ln -s $HOME/dotfiles/zshrc/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/zshrc/.zprofile $HOME/.zprofile
+
+# done
+echo "DONE"
